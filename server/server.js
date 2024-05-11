4b708c38fef2271d1359147fe7752e5c27cc47bb
tejasnasre <tejasnasre120@gmail.com>
2024-05-11 14:11:14 +0530

    Feature : Image Storage Of Canteen And FoodDish Completed

--- server/server.js
+++ server/server.js
@@ -1,37 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+const cloudinaryConfig = require("./config/cloudinaryConfig");
 
 app.use(
-    cors({
-      origin: "*",
-    })
+  cors({
+    origin: "*",
+  })
 );
 
 app.use(express.json());
+app.use("*", cloudinaryConfig.cloudinaryConfig);
 
 //mounting routes
-const studentRoutes = require("./routes/student"); 
+const studentRoutes = require("./routes/student");
 const canteenRoutes = require("./routes/canteen");
-app.use("/api/v1" , canteenRoutes);
+const uploadFileRouter = require("./routes/uploadFile");
+
+app.use("/api/v1", canteenRoutes);
 app.use("/api/v1", studentRoutes);
+app.use("/api/v1", uploadFileRouter);
 
-
-
-
-app.listen(PORT , ()=>{
-    console.log(`Server started succesfully at ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server started succesfully at ${PORT}`);
 });
-
 
 //getting connected to databse
 const dbConnect = require("./config/database");
 dbConnect();
 
-
 //default route
-app.get("/" , (req,res)=>{
-    res.send(`<h1>This is Homepage baby</h1>`);
-})
+app.get("/", (req, res) => {
+  res.send(`<h1>This is Homepage baby</h1>`);
+});
