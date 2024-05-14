bb6a34ac03d0bd18a1b341d24a71b7e6f626a0a1
Ashutosh <Ashutosh@LAPTOP-N00785I6>
2024-05-14 16:38:36 +0530

    Added Logout and Reset Password succesfully

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 4000;
 const cloudinaryConfig = require("./config/cloudinaryConfig");
 
@@ -9,7 +10,7 @@ app.use(
     origin: "*",
   })
 );
-
+app.use(cookieParser());
 app.use(express.json());
 app.use("*", cloudinaryConfig.cloudinaryConfig);
 
