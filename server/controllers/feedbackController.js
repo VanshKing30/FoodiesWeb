0f5a3f7c8fa7e41db8d37eb97fed3d3cbcdecaf9
~Chiluka Akshitha <120377576+AKSHITHA-CHILUKA@users.noreply.github.com>
2024-07-27 08:05:50 +0530

    Update feedbackController.js
    validation for message, canteenId, and userId fields and error Handling

--- server/controllers/feedbackController.js
+++ server/controllers/feedbackController.js
@@ -1,18 +1,22 @@
 const asyncHandler = require('express-async-handler');
-const Feedback = require("../models/studentfeeback");
+const Feedback = require('../models/studentfeedback');
 
 const submitFeedback = asyncHandler(async (req, res) => {
-  const { message, canteenId ,userId} = req.body;
+  const { message, canteenId, userId } = req.body;
 
-
-  if (!message || !canteenId) {
+  if (!message || !canteenId || !userId) {
     res.status(400);
-    throw new Error('Message and Canteen ID are required');
+    throw new Error('Message, Canteen ID, and User ID are required');
   }
 
-  const feedback = new Feedback({ message, canteenId, userId });
-  await feedback.save();
-  res.status(201).json({ message: 'Feedback submitted successfully' });
+  try {
+    const feedback = new Feedback({ message, canteenId, userId });
+    await feedback.save();
+    res.status(201).json({ message: 'Feedback submitted successfully' });
+  } catch (error) {
+    res.status(500);
+    throw new Error('Server error, could not submit feedback');
+  }
 });
 
 module.exports = { submitFeedback };
