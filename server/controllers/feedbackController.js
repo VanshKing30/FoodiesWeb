32e4428a673d4e2bc25ee9691c26587416642058
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-28 20:39:55 +0530

    Added Feedback system

--- server/controllers/feedbackController.js
+++ server/controllers/feedbackController.js
@@ -1,6 +1,19 @@
 const asyncHandler = require('express-async-handler');
 const Feedback = require("../models/studentfeeback");
 
+const getFeedbacksByCanteen = asyncHandler(async (req, res) => {
+  const { canteenId } = req.params;
+
+  if (!canteenId) {
+    res.status(400);
+    throw new Error('Canteen ID is required');
+  }
+
+  const feedbacks = await Feedback.find({ canteenId });
+
+  res.status(200).json(feedbacks);
+});
+
 const submitFeedback = asyncHandler(async (req, res) => {
   const { message, canteenId ,userId} = req.body;
 
@@ -15,4 +28,4 @@ const submitFeedback = asyncHandler(async (req, res) => {
   res.status(201).json({ message: 'Feedback submitted successfully' });
 });
 
-module.exports = { submitFeedback };
+module.exports = { submitFeedback ,getFeedbacksByCanteen };
--- server/routes/student.js
+++ server/routes/student.js
@@ -2,11 +2,8 @@ const express = require("express");
 const { auth, studentAuth, isCanteen } = require("../middlewares/auth");
 const router = express.Router();
 const authController = require("../controllers/Auth");
-<<<<<<< HEAD
 const { getCanteenData } = require("../controllers/canteenController");
-=======
 const feedbackController = require("../controllers/feedbackController");
->>>>>>> 56cd98b40c82e9849270fe2c43ef735b8fe058bc
 
 router.post("/studentSignup", authController.studentSignup);
 router.post("/studentLogin", authController.studentLogin);
@@ -17,12 +14,10 @@ router.get("/resetPassword/:id/:token", authController.verifyLink);
 router.post("/newPassword/:id/:token", authController.resetPassword);
 router.get("/studentLogout", studentAuth, authController.studentLogout);
 router.get("/canteenLogout", auth, authController.canteenLogout);
-<<<<<<< HEAD
 router.get("/canteen/:id", getCanteenData);
-=======
 router.post('/submitFeedback', feedbackController.submitFeedback);
+router.get('/feedbacks/:canteenId', feedbackController.getFeedbacksByCanteen);
 
->>>>>>> 56cd98b40c82e9849270fe2c43ef735b8fe058bc
 router.post(
   "/changeStudentPassword",
   studentAuth,
