5e3e4b151b22ae839c5dee4b93e4f6a9f54a8501
Saksham2k3s <sakshamshrivastav58@gmail.com>
2024-08-08 14:23:58 +0530

    Solved cors issue in server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // CORS Configuration
 app.use(
   cors({
-    origin: "http://localhost:3000", // Adjust this to match your frontend URL
+    origin: process.env.FRONTEND_URL, // Adjust this to match your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies to be sent with requests
