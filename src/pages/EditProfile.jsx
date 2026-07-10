96484568e11de665070e1416ff6309b27be511f4
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-10 12:24:29 +0530

    fix: remove console.log from EditProfile.jsx

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -109,7 +109,6 @@ const EditProfile = () => {
         }
       );
       const result = await response.json();
-      console.log(result);
       window.location.reload();
     } catch (error) {
       console.error("Error updating profile:", error);
