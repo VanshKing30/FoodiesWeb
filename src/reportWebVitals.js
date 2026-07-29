49d8b9d1d558a81df8c71895fa0db9e0a61c8afc
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-29 13:59:23 +0530

    fix: handle promise rejections with .catch()

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,7 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+.catch(err => console.error(err))
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
