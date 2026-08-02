c7acae0719e84b0b702b49ec0f0203a46bdc82ba
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-08-03 00:51:24 +0530

    fix: additional real bug fixes

--- src/components/OtpRequest.jsx
+++ src/components/OtpRequest.jsx
@@ -73,7 +73,7 @@ const OtpVerify = () => {
           <form onSubmit={handleSubmit} className="flex flex-col items-center w-full">
           <h1 className="text-2xl md:text-4xl font-serif mb-6 md:mb-10">OTP verification</h1>
             <div className="flex space-x-2">
-              {otp.map((data, index) => (
+              {(otp ?? []).map((data, index) => (
                 <input
                   key={index}
                   id={`otp-input-${index}`}
